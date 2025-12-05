76b7db2b0cd8a713c2a31e83b62d2385feee94ad
JonProg <jonashenri.aquino@gmail.com>
2025-12-05 11:12:15 -0300

    add: create interfaces for api

---
+++ src/interfaces/bookings.interface.ts
@@ -0,0 +1,7 @@
+export interface Booking {
+    id: string;
+    userId: string;
+    roomId: string;
+    startTime: Date;
+    endTime: Date;
+}   
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,7 @@
 import Fastify from 'fastify'
-import usersRoutes from './routes/users_routes.js';
 //import cors from '@fastify/cors'
 
 const app = Fastify({ logger: true })
-app.register(usersRoutes, {prefix:'/api'})
 
 const start = async () => {
     //await app.register(cors)
